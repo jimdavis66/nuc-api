efc8c5c1cb975b442e0d61e5904345e43bf91622
James Davis <james.norman.davis@gmail.com>
2018-03-15 18:43:18 +1100

    Add radio functionality

--- app.js
+++ app.js
@@ -16,6 +16,16 @@ app.get('/radio', (req, res, next) => {
   res.send('Radio');
 });
 
+app.post('/radio/:action', (req, res, next) => {
+  if(req.params.action === 'on') {
+    res.send('Turn radio on');
+  } else if (req.params.action === 'off') {
+    res.send('Turn radio off');
+  } else {
+    res.status(404).send();
+  }
+});
+
 app.listen(config.server.port, () => {
   console.log(`Server is listening on port ${config.server.port}`);
 });
